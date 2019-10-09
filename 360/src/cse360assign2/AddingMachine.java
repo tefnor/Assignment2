bf7e6cdb31e7f1bf9c5a806a834bb4a7df6fbad0
Tanner <tefnor@asu.edu>
2019-10-09 12:31:36 -0700

    Added comments

--- 360/src/cse360assign2/AddingMachine.java
+++ 360/src/cse360assign2/AddingMachine.java
@@ -1,29 +1,42 @@
+/* Name of Author: Tanner Efnor
+ * Class ID: 70641
+ * Assignment #2
+ * Description: Trying to learn how git and github work.
+ */
 package cse360assign2;
 
+//This class is not really finished at the moment for this portion of the assignment.
 public class AddingMachine {
 
+	//Private integer total
 	private int total;
 	
+	//Just sets total equal to zero in this method.
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
 	}
 	
+	//Blank method besides returning 0
 	public int getTotal () {
 		return 0;
 	}
 	
+	//Blank method does not do anything yet.
 	public void add (int value) {
 		
 	}
 	
+	//Blank method does not do anything yet.
 	public void subtract (int value) {
 		
 	}
 		
+	//Blank method does not do anything yet.
 	public String toString () {
 		return "";
 	}
 
+	//Blank method does not do anything yet.
 	public void clear() {
 	
 	}
